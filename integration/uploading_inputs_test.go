035a97dea14b7288c50181023b65abfcbe157fcc
Evan Short <eshort@pivotal.io>
2015-11-06 14:29:59 -0800

    WIP
    Signed-off-by: Corbin Halliwill <challiwill@pivotal.io>

---
+++ integration/uploading_inputs_test.go
@@ -0,0 +1,233 @@
+package integration_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/gexec"
+	"github.com/onsi/gomega/ghttp"
+	"github.com/vito/go-sse/sse"
+
+	"github.com/concourse/atc"
+	"github.com/concourse/atc/event"
+)
+
+var _ = Describe("Fly CLI", func() {
+	var tmpdir string
+	var buildDir string
+	var taskConfigPath string
+
+	var atcServer *ghttp.Server
+	var streaming chan struct{}
+	var events chan atc.Event
+	var uploadingBits <-chan struct{}
+
+	var expectedPlan atc.Plan
+
+	BeforeEach(func() {
+		var err error
+		tmpdir, err = ioutil.TempDir("", "fly-build-dir")
+		Expect(err).NotTo(HaveOccurred())
+
+		buildDir = filepath.Join(tmpdir, "fixture")
+
+		err = os.Mkdir(buildDir, 0755)
+		Expect(err).NotTo(HaveOccurred())
+
+		taskConfigPath = filepath.Join(buildDir, "task.yml")
+
+		err = ioutil.WriteFile(
+			taskConfigPath,
+			[]byte(`---
+platform: some-platform
+
+image: ubuntu
+
+inputs:
+- name: fixture
+
+params:
+  FOO: bar
+  BAZ: buzz
+  X: 1
+
+run:
+  path: find
+  args: [.]
+`),
+			0644,
+		)
+		Expect(err).NotTo(HaveOccurred())
+
+		atcServer = ghttp.NewServer()
+
+		streaming = make(chan struct{})
+		events = make(chan atc.Event)
+
+		expectedPlan = atc.Plan{
+			OnSuccess: &atc.OnSuccessPlan{
+				Step: atc.Plan{
+					Aggregate: &atc.AggregatePlan{
+						atc.Plan{
+							Location: &atc.Location{
+								ParallelGroup: 1,
+								ParentID:      0,
+								ID:            2,
+							},
+							Get: &atc.GetPlan{
+								Name: filepath.Base(buildDir),
+								Type: "archive",
+								Source: atc.Source{
+									"uri": atcServer.URL() + "/api/v1/pipes/some-pipe-id",
+								},
+							},
+						},
+					},
+				},
+				Next: atc.Plan{
+					Location: &atc.Location{
+						ParallelGroup: 0,
+						ParentID:      0,
+						ID:            3,
+					},
+					Task: &atc.TaskPlan{
+						Name: "one-off",
+						Config: &atc.TaskConfig{
+							Platform: "some-platform",
+							Image:    "ubuntu",
+							Inputs: []atc.TaskInputConfig{
+								{Name: "fixture"},
+							},
+							Params: map[string]string{
+								"FOO": "bar",
+								"BAZ": "buzz",
+								"X":   "1",
+							},
+							Run: atc.TaskRunConfig{
+								Path: "find",
+								Args: []string{"."},
+							},
+						},
+					},
+				},
+			},
+		}
+	})
+
+	AfterEach(func() {
+		err := os.RemoveAll(tmpdir)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	JustBeforeEach(func() {
+		uploading := make(chan struct{})
+		uploadingBits = uploading
+
+		atcServer.AppendHandlers(
+			ghttp.CombineHandlers(
+				ghttp.VerifyRequest("POST", "/api/v1/pipes"),
+				ghttp.RespondWithJSONEncoded(http.StatusCreated, atc.Pipe{
+					ID: "some-pipe-id",
+				}),
+			),
+		)
+		atcServer.RouteToHandler("POST", "/api/v1/builds",
+			ghttp.CombineHandlers(
+				ghttp.VerifyRequest("POST", "/api/v1/builds"),
+				ghttp.VerifyJSONRepresenting(expectedPlan),
+				func(w http.ResponseWriter, r *http.Request) {
+					http.SetCookie(w, &http.Cookie{
+						Name:    "Some-Cookie",
+						Value:   "some-cookie-data",
+						Path:    "/",
+						Expires: time.Now().Add(1 * time.Minute),
+					})
+				},
+				ghttp.RespondWith(201, `{"id":128}`),
+			),
+		)
+		atcServer.RouteToHandler("GET", "/api/v1/builds/128/events",
+			ghttp.CombineHandlers(
+				ghttp.VerifyRequest("GET", "/api/v1/builds/128/events"),
+				func(w http.ResponseWriter, r *http.Request) {
+					flusher := w.(http.Flusher)
+
+					w.Header().Add("Content-Type", "text/event-stream; charset=utf-8")
+					w.Header().Add("Cache-Control", "no-cache, no-store, must-revalidate")
+					w.Header().Add("Connection", "keep-alive")
+
+					w.WriteHeader(http.StatusOK)
+
+					flusher.Flush()
+
+					close(streaming)
+
+					id := 0
+
+					for e := range events {
+						payload, err := json.Marshal(event.Message{Event: e})
+						Expect(err).NotTo(HaveOccurred())
+
+						event := sse.Event{
+							ID:   fmt.Sprintf("%d", id),
+							Name: "event",
+							Data: payload,
+						}
+
+						err = event.Write(w)
+						Expect(err).NotTo(HaveOccurred())
+
+						flusher.Flush()
+
+						id++
+					}
+
+					err := sse.Event{
+						Name: "end",
+					}.Write(w)
+					Expect(err).NotTo(HaveOccurred())
+				},
+			),
+		)
+		atcServer.RouteToHandler("PUT", "/api/v1/pipes/some-pipe-id",
+			ghttp.CombineHandlers(
+				ghttp.VerifyRequest("PUT", "/api/v1/pipes/some-pipe-id"),
+				func(w http.ResponseWriter, req *http.Request) {
+					close(uploading)
+					atcServer.CloseClientConnections()
+				},
+			),
+		)
+	})
+
+	Context("when the atc goes offline while uploading inputs", func() {
+		FIt("exits 0", func() {
+			flyCmd := exec.Command(flyPath, "-t", atcServer.URL(), "e", "-c", taskConfigPath)
+			flyCmd.Dir = buildDir
+
+			sess, err := gexec.Start(flyCmd, GinkgoWriter, GinkgoWriter)
+			Expect(err).ToNot(HaveOccurred())
+
+			Eventually(streaming, 5).Should(BeClosed())
+
+			<-uploadingBits
+
+			//events <- event.Status{Status: atc.StatusSucceeded}
+			close(events)
+
+			<-sess.Exited
+			Expect(sess.ExitCode()).To(Equal(1))
+
+			Expect(sess.Err).Should(gbytes.Say("upload request failed"))
+		})
+	})
+})
